84c5928ded22474ded4847d59b03dede4ba24151
luke <luke.weisensee@gmail.com>
2019-11-13 15:15:07 -0800

    removes default styling

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,15 +1,8 @@
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import IconButton from '@material-ui/core/IconButton';
-import LockIcon from '@material-ui/icons/Lock';
 import React, { useEffect } from 'react';
 import Toolbar from '@material-ui/core/Toolbar';
-import Tooltip from '@material-ui/core/Tooltip';
 import Typography from '@material-ui/core/Typography';
-import { GitHubIcon } from 'rmw-shell/lib/components/Icons';
 import { Helmet } from 'react-helmet';
 import { withRouter } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
@@ -141,39 +134,12 @@ const LandingPage = ({ classes, history, theme }) => {
 			<AppBar position="static">
 				<Toolbar disableGutters>
 					<div style={{ flex: 1 }} />
-
-					<Tooltip id="tooltip-icon1" title="Sign in">
-						<IconButton
-							name="signin"
-							aria-label="Open Github"
-							color="inherit"
-							onClick={() => {
-								history.push('/signin');
-							}}
-							rel="noopener"
-						>
-							<LockIcon />
-						</IconButton>
-					</Tooltip>
-					<Tooltip id="tooltip-icon2" title="GitHub repository">
-						<IconButton
-							name="github"
-							aria-label="Open Github"
-							color="inherit"
-							href="https://github.com/TarikHuber/react-most-wanted"
-							target="_blank"
-							rel="noopener"
-						>
-							<GitHubIcon />
-						</IconButton>
-					</Tooltip>
 				</Toolbar>
 			</AppBar>
 
 			<div className={classes.root}>
 				<div className={classes.hero}>
 					<div className={classes.content}>
-						<img src="/rmw.svg" alt="Material-UI Logo" className={classes.logo} />
 						<div className={classes.text}>
 							<Typography
 								variant="h3"
@@ -183,7 +149,7 @@ const LandingPage = ({ classes, history, theme }) => {
 								gutterBottom
 								className={classes.title}
 							>
-								{'REACT MOST WANTED'}
+								{'G Code Generator'}
 							</Typography>
 							<Typography
 								variant="h5"
@@ -192,105 +158,16 @@ const LandingPage = ({ classes, history, theme }) => {
 								gutterBottom
 								className={classes.h5}
 							>
-								{'React Starter-Kit with all Most Wanted features.'}
+								{`Enter text, Press 'Generate'`}
 							</Typography>
 							<Button
-								onClick={() => {
-									history.push('/signin');
-								}}
+								onClick={this.generateGCode}
 								className={classes.button}
 								variant="outlined"
 								color="primary"
 							>
-								{'Get Started'}
+								{'Generate'}
 							</Button>
-						</div>
-
-						<div className={classes.cardsContent}>
-							<Card className={classes.card}>
-								<CardContent>
-									<Typography variant="h5" component="h2">
-										Installation
-									</Typography>
-									<br />
-									<Typography>{'Just run this script to start:'}</Typography>
-									<br />
-									<Typography className={classes.pos} color="textSecondary">
-										{' '}
-										npx create-react-app test-app --scripts-version
-										rmw-react-scripts{' '}
-									</Typography>
-								</CardContent>
-								<CardActions>
-									<Button
-										size="small"
-										onClick={() => {
-											var win = window.open(
-												'https://github.com/TarikHuber/rmw-shell',
-												'_blank'
-											);
-											win.focus();
-										}}
-									>
-										Learn More
-									</Button>
-								</CardActions>
-							</Card>
-							<Card className={classes.card}>
-								<CardContent>
-									<Typography variant="h5" component="h2">
-										Usage
-									</Typography>
-									<br />
-									<Typography>
-										{'Set your configuration to the App component:'}
-									</Typography>
-									<br />
-									<Typography className={classes.pos} color="textSecondary">
-										{"import App from 'rmw-shell'"}
-										<br />
-										{'<App appConfig={{ configureStore, ...config }} />'}
-									</Typography>
-								</CardContent>
-								<CardActions>
-									<Button
-										size="small"
-										onClick={() => {
-											var win = window.open(
-												'https://github.com/TarikHuber/react-most-wanted',
-												'_blank'
-											);
-											win.focus();
-										}}
-									>
-										Learn More
-									</Button>
-								</CardActions>
-							</Card>
-							<Card className={classes.card}>
-								<CardContent>
-									<Typography variant="h5" component="h2">
-										What is this?
-									</Typography>
-									<Typography noWrap={false} color="textSecondary">
-										{`This is a OPEN SOURCE demo application that demonstartes the usage of the rmw-shell library 
-                    with react, Material-UI and firebase.  `}
-										<br />
-										{` This demo has no purpose to do something as an app. 
-                    It is here just to show how everthing works together. `}
-									</Typography>
-								</CardContent>
-								<CardActions>
-									<Button
-										size="small"
-										onClick={() => {
-											history.push('/signin');
-										}}
-									>
-										Get started
-									</Button>
-								</CardActions>
-							</Card>
 						</div>
 					</div>
 				</div>
