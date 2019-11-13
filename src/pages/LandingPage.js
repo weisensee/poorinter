aef88aa61b2e8de877fdc85fd6a16a3df43da640
luke <luke.weisensee@gmail.com>
2019-11-13 15:27:50 -0800

    stubs out g code functions, cleans up imports

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,118 +6,8 @@ import Typography from '@material-ui/core/Typography';
 import { Helmet } from 'react-helmet';
 import { withRouter } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
-
-const styles = theme => ({
-	main: {
-		display: 'flex',
-		flexDirection: 'column'
-	},
-	root: {
-		flexGrow: 1,
-		flex: '1 0 100%'
-		// height: '100%',
-		// overflow: 'hidden'
-	},
-	hero: {
-		height: '100%',
-		// minHeight: '80vh',
-		flex: '0 0 auto',
-		display: 'flex',
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: theme.palette.background.paper,
-		color:
-			theme.palette.type === 'light' ? theme.palette.primary.dark : theme.palette.primary.main
-	},
-	text: {
-		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'center',
-		justifyContent: 'center'
-	},
-	title: {
-		letterSpacing: '.7rem',
-		textIndent: '.7rem',
-		fontWeight: theme.typography.fontWeightLight,
-		[theme.breakpoints.only('xs')]: {
-			fontSize: 24,
-			letterSpacing: '.1em',
-			textIndent: '.1rem'
-		},
-		whiteSpace: 'nowrap'
-	},
-	h5: {
-		paddingLeft: theme.spacing(1) * 4,
-		paddingRight: theme.spacing(1) * 4,
-		marginTop: theme.spacing(1),
-		maxWidth: 600,
-		textAlign: 'center',
-		[theme.breakpoints.only('xs')]: {
-			fontSize: 18
-		}
-	},
-	content: {
-		height: '100%',
-		// paddingTop: theme.spacing(1) * 8,
-		[theme.breakpoints.up('sm')]: {
-			paddingTop: theme.spacing(1)
-		}
-	},
-	button: {
-		marginTop: theme.spacing(1) * 3
-	},
-	logo: {
-		color: 'red',
-		margin: `${theme.spacing(1) * 3}px 0 ${theme.spacing(1) * 4}px`,
-		width: '100%',
-		height: '40vw',
-		maxHeight: 250
-	},
-	steps: {
-		maxWidth: theme.spacing(1) * 130,
-		margin: 'auto'
-	},
-	step: {
-		padding: `${theme.spacing(1) * 3}px ${theme.spacing(1) * 2}px`
-	},
-	stepIcon: {
-		marginBottom: theme.spacing(1)
-	},
-	markdownElement: {},
-	cardsContent: {
-		padding: 15,
-		display: 'flex',
-		justifyContent: 'space-around',
-		flexWrap: 'wrap',
-		[theme.breakpoints.only('xs')]: {
-			width: '100%',
-			padding: 0,
-			paddingTop: 15
-		}
-	},
-	card: {
-		minWidth: 275,
-		maxWidth: 350,
-		margin: 15,
-		[theme.breakpoints.only('xs')]: {
-			width: '100%',
-			margin: 0,
-			marginTop: 7
-		}
-	},
-	bullet: {
-		display: 'inline-block',
-		margin: '0 2px',
-		transform: 'scale(0.8)'
-	},
-	cardTitle: {
-		marginBottom: 16,
-		fontSize: 14
-	},
-	pos: {
-		marginBottom: 12
-	}
-});
+import { styles } from './styles';
+import { generateGCode } from '../utils/gCode';
 
 const LandingPage = ({ classes, history, theme }) => {
 	return (
@@ -161,7 +51,7 @@ const LandingPage = ({ classes, history, theme }) => {
 								{`Enter text, Press 'Generate'`}
 							</Typography>
 							<Button
-								onClick={this.generateGCode}
+								onClick={generateGCode}
 								className={classes.button}
 								variant="outlined"
 								color="primary"
---
+++ src/utils/gCode.ts
@@ -0,0 +1,3 @@
+export function generateGCode() {
+	console.log('g code');
+}
