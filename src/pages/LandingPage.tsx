5c7f3089cbf1f5e54b206fd0dd9ca098b261dd1d
luke <luke.weisensee@gmail.com>
2019-11-13 16:00:04 -0800

    stubs out G code compiler

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,121 @@
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import React, { Component } from 'react';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { Helmet } from 'react-helmet';
+import { withRouter } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+import { styles } from './styles';
+import { generateGCode } from '../utils/gCode';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+
+interface Props {
+	classes: any;
+	history: any;
+	theme: any;
+}
+interface State {
+	gCode: string;
+	text: string;
+}
+
+class LandingPage extends Component<Props, State> {
+	constructor(props: Props) {
+		super(props);
+		this.state = { text: '', gCode: '' };
+	}
+
+	// const [name, setText] = React.useState('');
+
+	handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		this.setState({ text: event.target.value });
+	};
+	generateGCode = () => {
+		this.setState({ gCode: generateGCode(this.state.text) });
+	};
+	render() {
+		const { classes, history, theme } = this.props;
+		const { gCode } = this.state;
+		return (
+			<div className={classes.main}>
+				<Helmet>
+					<meta name="theme-color" content={theme.palette.primary.main} />
+					<meta
+						name="apple-mobile-web-app-status-bar-style"
+						content={theme.palette.primary.main}
+					/>
+					<meta
+						name="msapplication-navbutton-color"
+						content={theme.palette.primary.main}
+					/>
+					<title>G Code Generator</title>
+				</Helmet>
+				<AppBar position="static">
+					<Toolbar disableGutters>
+						<div style={{ flex: 1 }} />
+					</Toolbar>
+				</AppBar>
+
+				<div className={classes.root}>
+					<div className={classes.hero}>
+						<div className={classes.content}>
+							<div className={classes.text}>
+								<Typography
+									variant="h3"
+									align="center"
+									component="h1"
+									color="inherit"
+									gutterBottom
+									className={classes.title}
+								>
+									{'G Code Generator'}
+								</Typography>
+								<Typography
+									variant="h5"
+									component="h2"
+									color="inherit"
+									gutterBottom
+									className={classes.h5}
+								>
+									{`Enter text, Press 'Generate'`}
+								</Typography>
+								<TextField
+									id="outlined-basic"
+									required
+									className={classes.textField}
+									label="Tweet Text"
+									margin="normal"
+									onChange={this.handleChange}
+									variant="outlined"
+								/>
+								<Button
+									onClick={this.generateGCode}
+									className={classes.button}
+									variant="outlined"
+									color="primary"
+								>
+									{'Generate'}
+								</Button>
+								{gCode.length ? (
+									<Paper
+										className={classes.root}
+										style={{ marginTop: '5rem', padding: '2rem' }}
+									>
+										<Typography variant="h5" component="h3">
+											Generated G Code:
+										</Typography>
+										<Typography component="p">{gCode}</Typography>
+									</Paper>
+								) : null}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+// @ts-ignore
+export default withRouter(withStyles(styles, { withTheme: true })(LandingPage));
--- src/utils/gCode.ts
+++ src/utils/gCode.ts
@@ -1,3 +1,3 @@
-export function generateGCode() {
-	console.log('g code');
+export function generateGCode(text: string) {
+	return `This is G Code: ${text}`;
 }
